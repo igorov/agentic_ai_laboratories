6bfc5c9d59ab49fa129237dc7093094662e01c60
Ernesto Laura <ernesto.laura@interseguro.com.pe>
2026-09-19 02:38:48 -0500

    base

--- assistant_base_01/src/utils/environment.py
+++ assistant_base_01/src/utils/environment.py
@@ -1,6 +1,15 @@
 from decouple import config
 
+# Nivel de logs
 LOG_LEVEL: str = config("LOG_LEVEL", default="INFO")
 
+# Acceso a la BD
 DATABASE_URL: str = config("DATABASE_URL", default="postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
 
+# OpenAI
+OPENAI_API_KEY: str = config("OPENAI_API_KEY")
+
+# Qdrant
+QDRANT_URL: str = config("QDRANT_URL")
+QDRANT_KEY: str = config("QDRANT_KEY")
+QDRANT_COLLECTION_NAME: str = config("QDRANT_COLLECTION_NAME")
